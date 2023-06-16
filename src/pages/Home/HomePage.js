8171592a4ce52293f75d270943d8ce03d03a03a4
Dan <webdevd4n@gmail.com>
2023-06-16 18:20:08 +0100

    Confetti added

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Confetti from "../../components/Confetti/Confetti";
+
 function HomePage() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function fireConfetti() {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 2);
+    setIsVisible(false);
+  }
+
   return (
     <div className="main__page">
       <div className="cont--max">home page</div>
+      {/* <button onClick={() => setIsVisible(true)}>Fire</button>
+      {isVisible && <Confetti />} */}
+      <button onClick={() => fireConfetti()}>Fire</button>
+      {isVisible && <Confetti />}
     </div>
   );
 }
--- src/pages/Routine/RoutinePage.js
+++ src/pages/Routine/RoutinePage.js
@@ -2,21 +2,13 @@
 import "./RoutinePage.css";
 
 // Components
-import RoutineEntry from "../../blocks/RoutineEntry/RoutineEntry";
 
 function RoutinePage() {
   return (
     <div className="main__page">
       <div className="cont--max">
         <h1>Routine page</h1>
-        <div className="main__page__inner">
-          <div className="routine-container disp--flex flex--colu">
-            <RoutineEntry />
-            <RoutineEntry />
-            <RoutineEntry />
-            <RoutineEntry />
-          </div>
-        </div>
+        <div className="main__page__inner">routine</div>
       </div>
     </div>
   );
